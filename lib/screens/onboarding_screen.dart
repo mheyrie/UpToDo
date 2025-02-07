77063dbf1b2013d273b5a67cb9669d005958a904
Mheyrie <omota4fidelity@gmail.com>
2025-02-07 12:34:59 +0100

    adding onboarding sceen

---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyOnboarding extends StatefulWidget {
+  const MyOnboarding({super.key});
+
+  @override
+  State<MyOnboarding> createState()=> _MyOnboardingState();
+  
+}
+
+class _MyOnboardingState extends State<MyOnboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
