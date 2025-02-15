c6a77cdda5c13b73b1a72f8746e934aadada17b3
Mheyrie <omota4fidelity@gmail.com>
2025-02-15 17:59:58 +0100

    connected splash scree to onboarding

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -12,8 +12,10 @@ class _MyOnboardingState extends State<MyOnboarding> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Center(
-        child: Text('Onboarding Screen'),
+        child: Text('This is the Onboarding Screen',
+        style: TextStyle(color:Colors.white, fontSize:24)),
       ),
     );
   }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:uptodo/screens/onboarding_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,8 +11,19 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    Timer(Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MyOnboarding()),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -19,7 +32,9 @@ class _SplashScreenState extends State<SplashScreen> {
             Text(
               'Uptodo',
               style: TextStyle(
-                  fontSize: 35, fontWeight: FontWeight.bold, color: Colors.white),
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
             ),
           ],
         ),
