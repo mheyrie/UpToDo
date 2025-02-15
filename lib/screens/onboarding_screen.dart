26a46b75fa1ea917738a1dafe5216c8305ed0be3
Mheyrie <omota4fidelity@gmail.com>
2025-02-15 18:10:26 +0100

    PageView updated

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -4,19 +4,23 @@ class MyOnboarding extends StatefulWidget {
   const MyOnboarding({super.key});
 
   @override
-  State<MyOnboarding> createState()=> _MyOnboardingState();
-  
+  State<MyOnboarding> createState() => _MyOnboardingState();
 }
 
 class _MyOnboardingState extends State<MyOnboarding> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Text('This is the Onboarding Screen',
-        style: TextStyle(color:Colors.white, fontSize:24)),
+        body: PageView(children: [
+      Container(
+        color: Colors.black,
       ),
-    );
+      Container(
+        color: Colors.blue,
+      ),
+      Container(
+        color: Colors.red,
+      ),
+    ]));
   }
 }
